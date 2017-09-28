8027c837b177861e3dba4331faf23bce85364ee9
ryan manthorp <ryanmanthorp@hotmail.co.uk>
2017-09-28 12:28:50 +0100

    Initial commit

---
+++ ex4.py
@@ -0,0 +1,16 @@
+my_name = 'Michael Davis'
+my_age = 26
+my_height = 70
+my_weight = 200
+my_eyes = 'Brown'
+my_hair = 'Dark Brown'
+is_heavy = my_weight > 3000
+
+print(f"Let's talk about {my_name}.")
+print(f"He is {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"It is {is_heavy} that he is big and strong")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If a add {my_age}, {my_height} and {my_weight} I get {total}")
