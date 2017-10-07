8fb8587c2c8543bb7176c2e172f679d2007dd213
Michael Davis <MichaelRDavis1991@protonmail.com>
2017-10-07 11:38:46 +0100

    Add spacing to code, added more comments

--- Pyhton/Week 2/ex6.py
+++ Pyhton/Week 2/ex6.py
@@ -4,24 +4,33 @@ Name = input("Enter your name:")
 print("Hello", Name)
 if(Name == "Chris Janes"):
     print("Hello tutor!")
+    
 #Age
 Age = 0
 Age = int(input("Enter your age:"))
 print("You are", Age, "year's old")
     if(Age <= 0):
         print("You can't be that old")
+
 #Height
 Height = 0
 Height = float(input("Enter your height in centimeters"))
 print("You are", Height, "centimeters tall")
+    if(Height > 215):
+        print("WOW! You're tall!")
+
 #Weight
 Weight = 0
 Weight = float(input("Enter your weight in kilograms"))
 print("Your weight is", Weight, "kilograms")
+    if(Weight > 150):
+        print("I think you need to go on a diet!")
+
 #Eye color
 EyeColor = "/0"
 EyeColor = input("Enter your eye color")
 print("Your eyes are", EyeColor)
+
 #Hair color
 HairColor = "/0"
 HairColor = input("Enter your hair color")
