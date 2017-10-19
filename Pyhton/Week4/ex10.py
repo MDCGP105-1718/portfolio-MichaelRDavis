a873a20edba0bc9560002602d36d2a56ca64fd28
MichaelRDavis <MichaelRDavis1991@protonmail.com>
2017-10-19 11:09:29 +0100

    Commit changes to master

--- Pyhton/Week4/ex10.py
+++ Pyhton/Week4/ex10.py
@@ -1,5 +1,5 @@
-low = int(input("Insert a low number:"))
-high = int(input("Insert a high number:"))
+low = int(input("Insert a low number:\n"))
+high = int(input("Insert a high number:\n"))
 
 def FizzBuzz(low, high):
     for n in range(low, high):
@@ -7,6 +7,6 @@ def FizzBuzz(low, high):
             print("Fizz")
         if(n % 5 == 0):
             print("Buzz")
-        if(n % 5 and n % 3 == 0):
+        if(n % 5 == 0 and n % 3 == 0):
             print("FizzBuzz")
 FizzBuzz(low, high)
